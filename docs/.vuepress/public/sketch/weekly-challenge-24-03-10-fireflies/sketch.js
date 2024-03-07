885fbce5f0594067d52d12e03021ebe87e95ec7e
Oz Ramos <heyozramos@gmail.com>
2024-03-06 18:16:42 -0800

    #0 Adds light arcs and ambiently lit dust

--- docs/.vuepress/public/sketch/weekly-challenge-24-03-10-fireflies/sketch.js
+++ docs/.vuepress/public/sketch/weekly-challenge-24-03-10-fireflies/sketch.js
@@ -33,11 +33,55 @@ Layers.ready(() => {
       }
 
       // random amount of fireflies as circles
+      const fireflies = []
       brush.noStroke()
       for (let i = 0; i < random(25, 50); i++) {
-        brush.fill(random(150, 255), random(150, 255), 0, random(100, 255))
-        brush.circle(random(-width/1.75, width/1.75), random(-height/1.75, height/1.75), random(minSize*.005, minSize*.025))
+        const x = random(-width/1.75, width/1.75)
+        const y = random(-height/1.75, height/1.75)
+        const size = random(minSize*.005, minSize*.025)
+
+        const col = random(150, 255) 
+        
+        for (let j = 0; j < 3; j++) {
+          brush.fill(col+j*10, col+j*10, j*90, random(100, 255))
+          brush.circle(x, y, size - j*minSize*.0025)
+        }
+
+        fireflies.push({x, y, size})
       }
+
+      fireflies.forEach(({x, y, size}) => {
+        // Firefly effects
+        switch (~~random(4)) {
+          // Glow with rings
+          case 1:
+            brush.field('seabed')
+            brush.field('curved')
+            brush.noFill()
+            
+            // Translate to firefly position
+            for (let k = 0; k < ~~random(4, 20); k++) {
+              let r = size + random(minSize * 0.01, minSize * 0.05)
+              let startAngle = random(PI*2)
+              let endAngle = startAngle + random(PI)
+      
+              if (random(6) > 2) {
+                const col = random(155, 255)
+                brush.stroke(col, col, 0)
+                brush.strokeWeight(random(3, 9))
+                brush.pick('pen')
+              } else {
+                const col = random(100, 200) 
+                brush.stroke(col, col, 0)
+                brush.strokeWeight(random(1, 6))
+                brush.pick('spray')
+              }
+      
+              brush.arc(x, y, r, startAngle, endAngle)
+            }
+          break
+        }
+      })
 
       brush.reDraw()
       brush.reBlend()
