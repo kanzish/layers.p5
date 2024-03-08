13f7b5101331f47a0b64f87adffcc6e51baa98ff
Oz Ramos <heyozramos@gmail.com>
2024-03-08 13:10:07 -0800

    #0 1st draft for weekly challenge - fireflies

--- docs/.vuepress/public/sketch/weekly-challenge-24-03-10-fireflies/sketch.js
+++ docs/.vuepress/public/sketch/weekly-challenge-24-03-10-fireflies/sketch.js
@@ -8,7 +8,15 @@ Layers.ready(() => {
       background(0)
       brush.load(this.canvas)
       brush.noStroke()
+      brush.noField()
 
+      // Jar placement
+      const jarWidth = random(minSize*.25, minSize*.45)
+      const jarHeight = random(minSize*.45, minSize*.75)
+      const jarX = random(-width/2, 0)
+      const jarY = random(height/3, height/1.5)-jarHeight/2
+
+      
       // Fullscreen watercolor
       // - Creates the effect of mountains and clouds
       brush.fill(50, 50, random(155, 200), 255)
@@ -53,7 +61,7 @@ Layers.ready(() => {
       brush.field('seabed')
       brush.field('curved')
       for (let n = 0; n < 3; n++) {
-        let grassCount = 200-n*50
+        let grassCount = 200-n*30
         for (let i = 0; i < grassCount; i++) {
           if (!n && random()>.25) {
             brush.pick('marker')
@@ -78,12 +86,12 @@ Layers.ready(() => {
 
 
 
-      // Fireflies
+      // Normal Fireflies
       // - some will glow
       // - illuminates environment dust
       const fireflies = []
       brush.noStroke()
-      for (let i = 0; i < random(80, 150); i++) {
+      for (let i = 0; i < random(80, 200); i++) {
         const x = random(-width/1.75, width/1.75)
         const y = random(-height/1.75, height/1.75)
         let size = random() > .75 ? random(minSize*.005, minSize*.025) : random(minSize*.0025, minSize*.015)
@@ -100,51 +108,167 @@ Layers.ready(() => {
         }
       }
 
-      fireflies.forEach(({x, y, size}) => {
-        // Firefly effects
-        switch (~~random(4)) {
-          // Glow with rings
-          case 1:
-            brush.field('seabed')
-            brush.field('curved')
-            brush.noFill()
-            
-            let hasCreatedStars = false
-            for (let k = 0; k < ~~random(6, 30); k++) {
-              let r = size + random(minSize * 0.01, minSize * 0.05)
-              let startAngle = random(PI*2)
-              let endAngle = startAngle + random(PI)
-      
-              brush.pick('pen')
-              if (random(8) > 2) {
-                const col = random(155, 255)
-                brush.stroke(col, col, 0)
-                brush.strokeWeight(random(1, 9))
-              } else {
-                const col = random(100, 200) 
-                
-                // Add 4 lines in star shape around center
-                if (!hasCreatedStars && random() > .5) {
-                  brush.stroke(col+100, col+100, 255)
-                  const angle = random(PI*2)
-                  for (let a = 0; a < 4; a++) {
-                    brush.strokeWeight(random(1, 6))
-                    const len = random(size*1.5, size*2.5)
-                    brush.line(x - cos(angle + a*PI*.25)*len, y - sin(angle + a*PI*.25)*len, x + cos(angle + a*PI*.25)*len, y + sin(angle + a*PI*.25)*len)
-                  }
-                  hasCreatedStars = true
-                }
 
+
+
+
+      // Glass jar
+      const jarOpacity = random(60, 150)
+      brush.pick('pen')
+      brush.fill(155, 155, 255, jarOpacity)
+      
+      // body
+      // border
+      brush.noField()
+      brush.stroke(50+jarOpacity, 50+jarOpacity, 50+jarOpacity)
+      brush.strokeWeight(max(1, random(minSize*.001, minSize*.005)))
+      brush.rect(jarX, jarY, jarWidth, jarHeight)
+      brush.strokeWeight(max(1, random(minSize*.001, minSize*.005)))
+      brush.rect(jarX, jarY, jarWidth, jarHeight)
+
+      // fill
+      brush.noStroke()
+      brush.rect(jarX, jarY, jarWidth, jarHeight)
+
+      // lid
+      const lidWidth = jarWidth*random(.6, .9)
+      const lidHeight = jarHeight*random(.15, .3)
+      brush.stroke(50+jarOpacity, 50+jarOpacity, 50+jarOpacity)
+      brush.rect(jarX+(jarWidth-lidWidth)/2, jarY-lidHeight*1.0, lidWidth, lidHeight)
+      brush.rect(jarX+(jarWidth-lidWidth)/2, jarY-lidHeight*1.0, lidWidth, lidHeight)
+
+      // White moon reflection lines inside jar
+      brush.stroke(255, 255, 255)
+
+      // left side reflections
+      for (let y = 0; y < random(0, 3); y++) {
+        let reflectionY = random(jarY, jarY+jarHeight*.8)
+        for (let n = 0; n < random(5, 10); n++) {
+          if (random() > .5) {
+            brush.pick('marker2')
+            brush.strokeWeight(random(1, 2))
+          } else {
+            brush.pick('pen')
+            brush.strokeWeight(random(3, 8))
+          }
+          
+          brush.field('seabed')
+          brush.line(jarX, reflectionY+random(-jarHeight*.05, jarHeight*.05), jarX+random(jarWidth*.5), reflectionY)
+        }
+      }
+
+      // right side reflections
+      for (let y = 0; y < random(0, 3); y++) {
+        let reflectionY = random(jarY, jarY+jarHeight*.8)
+        for (let n = 0; n < random(5, 10); n++) {
+          if (random() > .5) {
+            brush.pick('marker2')
+            brush.strokeWeight(random(1, 2))
+          } else {
+            brush.pick('pen')
+            brush.strokeWeight(random(1, 4))
+          }
+          
+          brush.field('seabed')
+          brush.line(jarX+jarWidth, reflectionY+random(-jarHeight*.05, jarHeight*.05), jarX+jarWidth-random(jarWidth*.5), reflectionY)
+        }
+      }
+
+
+      // Firefirelies inside jar
+      for (let i = 0; i < random(6, 30); i++) {
+        const x = jarX + random(jarWidth*.2, jarWidth*.8)
+        const y = jarY + random(-lidHeight*1.5, jarHeight*.5)
+        let size = random() > .75 ? random(minSize*.005, minSize*.025) : random(minSize*.0025, minSize*.015)
+
+        // Paint the spots 
+        const col = random(150, 255) 
+        for (let j = 0; j < 3; j++) {
+          brush.fill(col+j*10, col+j*10, j*90, random(100, 255))
+          brush.circle(x, y, size - j*minSize*.0025)
+        }
+
+        if (size > minSize*.01) {
+          fireflies.push({x, y, size, inJar: true})
+        }
+      }      
+
+      fireflies.forEach(({x, y, size, inJar = false}) => {
+        // Firefly effects
+        // Glow with rings
+        if (~~random(4) === 1 || inJar) {
+          brush.field('curved')
+          brush.noFill()
+          
+          let hasCreatedStars = false
+          for (let k = 0; k < ~~random(6, 20); k++) {
+            let r = size + random(minSize * 0.01, minSize * 0.05)
+            let startAngle = random(PI*2)
+            let endAngle = startAngle + random(PI)
+    
+            brush.pick('pen')
+            if (random(8) > 2 && !inJar) {
+              const col = random(155, 255)
+              brush.stroke(col, col, 0)
+              brush.strokeWeight(random(1, 9))
+            } else {
+              const col = random(100, 200) 
+              
+              // Add 4 lines in star shape around center
+              if (!hasCreatedStars && random() > .5) {
+                brush.stroke(col+100, col+100, 255)
+                const angle = random(PI*2)
+                for (let a = 0; a < 4; a++) {
+                  brush.strokeWeight(random(1, 6))
+                  const len = random(size*1.5, size*2.5)
+                  brush.line(x - cos(angle + a*PI*.25)*len, y - sin(angle + a*PI*.25)*len, x + cos(angle + a*PI*.25)*len, y + sin(angle + a*PI*.25)*len)
+                }
+                hasCreatedStars = true
+              }
+
+              if (inJar) {
+                brush.stroke(col+50, col+50, 0)
+                brush.strokeWeight(random(1, 6))
+                brush.pick('spray')
+              } else {
                 brush.stroke(col, col, 0)
                 brush.strokeWeight(random(1, 6))
                 brush.pick('spray')
               }
-      
-              brush.arc(x, y, r, startAngle, endAngle)
             }
-          break
+    
+            brush.arc(x, y, r, startAngle, endAngle)
+          }
         }
       })
+
+
+      // Grass
+      // - n "layers" of grass
+      // - each layer gets shorter but brighter
+      // - occasionally a blade will catch the light as yellow
+      brush.field('seabed')
+      brush.field('curved')
+      for (let n = 0; n < 1; n++) {
+        let grassCount = 200-n*30
+        for (let i = 0; i < grassCount; i++) {
+          brush.pick('pen')
+          brush.strokeWeight(random(minSize*.0025, minSize*.025))
+          
+          if (n < 2 && random(25) < 1) {
+            const col = random(100, 200)
+            brush.stroke(col, col, 0)
+          } else {
+            brush.stroke(random(10, 40)+n*8, random(30, 60)+n*12, 0)
+          }
+
+          const initAngle = PI*.5+random(-PI*.25, PI*.25)
+          brush.beginStroke('curve', width/2-i/grassCount*width, height/2)
+          brush.segment(initAngle, random(minSize*.01, minSize*.2), 1)
+          brush.endStroke(initAngle + random(-PI*.35, PI*.35))
+        }
+      }
+
 
       brush.reDraw()
       brush.reBlend()
